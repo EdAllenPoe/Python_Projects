0e454e4c00cba715ed25dd6241c96bf5b7c5874f
EdAllenPoe <eric@ericgott.com>
2017-07-30 14:32:46 -0700

    Array Compare Code Complete

---
+++ Array_Compare.py
@@ -0,0 +1,23 @@
+# list_one = [1,2,5,6,2]
+# list_two = [1,2,5,6,2]
+#
+# list_one = [1,2,5,6,5]
+# list_two = [1,2,5,6,5,3]
+#
+# list_one = [1,2,5,6,5,16]
+# list_two = [1,2,5,6,5]
+
+list_one = ['celery','carrots','bread','milk']
+list_two = ['celery','carrots','bread','cream']
+
+
+def arraytest (x,y):
+
+    if x==y:
+        print"WooHoo!! You got a match"
+
+    else:
+        print"Hey!! This doesn't match!!"
+
+
+arraytest(list_one,list_two)
