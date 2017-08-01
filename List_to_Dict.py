0ca76df3a0bb955c2ecd8322ef4885aa344add58
EdAllenPoe <eric@ericgott.com>
2017-08-01 16:24:44 -0700

    Dojo Assignments Take 1

---
+++ List_to_Dict.py
@@ -0,0 +1,8 @@
+name = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
+favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas"]
+
+favanimals = zip(name, favorite_animal)
+
+favanimals=dict(favanimals)
+
+print (favanimals)
---
+++ Name.py
@@ -0,0 +1,47 @@
+
+# Part One
+
+# students = [
+#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#      {'first_name' : 'John', 'last_name' : 'Rosales'},
+#      {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#      {'first_name' : 'KB', 'last_name' : 'Tonel'}
+# ]
+#
+# for x in students:
+#     print x['first_name'], x['last_name']
+#
+
+# Part Two
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+
+for key, data in users.items():
+     #print data
+     for value in data:
+          print value['first_name'], value['last_name'], len(value['last_name'])+len(value['first_name'])
+
+
+
+
+
+
+
+# for s in students:
+#     print x['first_name'], x['last_name']
+#
+#
+# for i in Instructors:
+#     print x['first_name'], x['last_name']
