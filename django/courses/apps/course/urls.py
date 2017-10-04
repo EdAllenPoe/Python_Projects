c9fd69fbe8a38575ef54d179185a7a0f47f72c16
EdAllenPoe <eric@ericgott.com>
2017-10-04 16:58:03 -0700

    First

---
+++ django/courses/apps/course/urls.py
@@ -0,0 +1,10 @@
+
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^course/create$', views.create),
+    url(r'^course/(?P<course_id>\d+)/confirm$', views.confirm),
+    url(r'^course/(?P<course_id>\d+)/delete$', views.delete)
+]
